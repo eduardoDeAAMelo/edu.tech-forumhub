8b1bd7267424183c52ffcb09a247c0afc47bfbb5
eduardoDeAAMelo <eduardo.melo0802@gmail.com>
2025-07-22 12:36:18 -0300

    inicializando projeto

---
+++ src/test/java/br/com/edutech/forumhubapi/ForumhubapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edutech.forumhubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumhubapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
